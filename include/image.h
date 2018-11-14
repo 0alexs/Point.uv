6f1bbd9133a667321b8cbf3f141a19ff431566ea
alexs <45044512+0alexs@users.noreply.github.com>
2018-11-14 17:49:39 +0000

    Create image.h

---
+++ include/image.h
@@ -0,0 +1,17 @@
+typedef unsigned char byte;
+typedef byte point;
+typedef unsigned int u_int;
+
+typedef struct { 
+	u_int x, y; 
+} coords;
+
+typedef struct { 
+	byte * ptr;
+	coords size;
+} image;
+
+#define GET_BPOS(x, _pos) (x & (0x1 << (7 - _pos)))
+#define BIT_CONV(x) (x && x)
+
+point get_point(coords * pos, image * img);
