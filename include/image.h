12dc18c6512c8acd015b156bc0e3bda2253eab64
alexs <45044512+0alexs@users.noreply.github.com>
2018-11-14 18:40:17 +0000

    rm useless code

--- include/image.h
+++ include/image.h
@@ -18,7 +18,3 @@ point get_point(coords * pos, image * img);
 int set_point(coords * pos, image * img, point p);
 
 int convert(image * ptr);
-
-typedef unsigned BOOL;
-#define TRUE 1
-#define FALSE 2
